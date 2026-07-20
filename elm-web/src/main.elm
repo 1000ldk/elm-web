a5bf156b0de090d9806e91561688ffd31d14c1ec
ldk <himzoru@gmail.com>
2026-07-20 17:59:00 +0900

    Refactor Elm application: Enhance state management and add Reset functionality
    - Updated the Model type alias for clarity.
    - Fixed the initialization of the initialModel.
    - Added a Reset message to the Msg type for resetting the count.
    - Refactored the update function to handle the new Reset message.
    - Improved the view function for better readability and structure.

--- elm-web/src/main.elm
+++ elm-web/src/main.elm
@@ -1,37 +1,51 @@
+module Main exposing (Model, Msg(..), initailModel, main, update, view)
+
 import Browser
-import Html exposing(Html, button, div, text)
+import Html exposing (Html, button, div, text)
 import Html.Events exposing (onClick)
 
-type alias Model = {count:Int}
 
-initailModel: Model
-initailModel = {count=0}
+type alias Model =
+    { count : Int }
+
+
+initailModel : Model
+initailModel =
+    { count = 0 }
+
 
 type Msg
-= Increment
-| Decrement
+    = Increment
+    | Decrement
+    | Reset
 
-update:Msg->Model->Model
-update msg model=
-case msg of 
-Increment->
-{model|count=model.count+1}
 
-Decrement->
-{model|count=model.count-1}
+update : Msg -> Model -> Model
+update msg model =
+    case msg of
+        Increment ->
+            { model | count = model.count + 1 }
 
-view:Model->Html Msg
-view model=
-div[]
-[button[onClick Decrement][test"-"]
-div[][text(String.fromInt model.count)]
-button[onClick Increment][test"+"]
-]
+        Decrement ->
+            { model | count = model.count - 1 }
 
-main:Program()Model Msg
-main=
-Browser.sandbox
-{init=initailModel
-view=view
-update=update
-}
+        Reset ->
+            { model | count = 0 }
+
+
+view : Model -> Html Msg
+view model =
+    div []
+        [ button [ onClick Decrement ] [ text "-" ]
+        , div [] [ text (String.fromInt model.count) ]
+        , button [ onClick Increment ] [ text "+" ]
+        ]
+
+
+main : Program () Model Msg
+main =
+    Browser.sandbox
+        { init = initailModel
+        , view = view
+        , update = update
+        }
