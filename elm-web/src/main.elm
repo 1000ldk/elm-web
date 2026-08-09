4abceecfb3adeec6098993a88c94547833806ed7
ldk <himzoru@gmail.com>
2026-08-09 21:33:44 +0900

    Center align the text in the main view

--- elm-web/src/main.elm
+++ elm-web/src/main.elm
@@ -3,6 +3,7 @@ module Main exposing (Model, Msg(..), initailModel, main, update, view)
 import Browser
 import Html exposing (Html, button, div, text)
 import Html.Events exposing (onClick)
+import Html.Attributes exposing (style)
 
 
 type alias Model =
@@ -37,7 +38,7 @@ view : Model -> Html Msg
 view model =
     div []
         [ 
-          div [] [text "任意の文字列"]
+          div [style "text-align" "center"] [text "任意の文字列"]
         , button [ onClick Decrement ] [ text "-" ]
         , div [] [ text (String.fromInt model.count) ]
         , button [ onClick Increment ] [ text "+" ]
