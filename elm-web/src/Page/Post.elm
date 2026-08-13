a3fffd3152ba6baf5e5c9da3562ebbbc993025aa
ldk <himzoru@gmail.com>
2026-08-13 23:18:19 +0900

    Refactor Post and Route modules: simplify viewPost function and remove unused ElmBlog route

--- elm-web/src/Page/Post.elm
+++ elm-web/src/Page/Post.elm
@@ -1,45 +1,23 @@
 module Page.Post exposing (viewPost, Msg)
 
-import Browser
 import Html exposing (..)
-import Url exposing (Url)
 import Html.Attributes exposing (href)
-import Browser.Navigation as Nav
-import Route exposing (Route(..))
 import Article.Articles.R8.ElmBlog as ElmBlog
 
-type alias Model =
-     {
-        key : Nav.Key
-        ,route : Route
-     }
+type Msg
+    = GotElmBlogMsg ElmBlog.Msg
 
-type Msg 
-    = LinkClicked Browser.UrlRequest
-    | UrlChanged Url
-    | GotElmBlogMsg ElmBlog.Msg
-
-init : () -> Url -> Nav.Key -> (Model , Cmd Msg)
-init _ url key =
-    ( { key = key, route = Route.parseUrl url }, Cmd.none)
-
-update : Msg -> Model -> (Model , Cmd Msg)
-update msg model =
-    case msg of
-        GotElmBlogMsg _->
-           (model, Cmd.none)
-
-viewPost : Route -> Int -> Html Msg
+viewPost : Int -> Html Msg
 viewPost id =
     div []
-        [ a[href "/"][ text "←　一覧に戻る"]
-        , h1 [] [text ("記事"++ String.fromInt id)]
+        [ a [ href "/" ] [ text "←　一覧に戻る" ]
+        , h1 [] [ text ("記事" ++ String.fromInt id) ]
         , div []
-              [ case route of
-                  ElmBlog ->
+              [ case id of
+                  1 ->
                     Html.map GotElmBlogMsg ElmBlog.view.content
 
                   _ ->
-                  text ""  
+                    text "記事が見つかりません"
               ]
         ]
--- elm-web/src/Route.elm
+++ elm-web/src/Route.elm
@@ -6,7 +6,6 @@ import Url.Parser as Parser exposing (Parser, (</>), int, map, oneOf, s, top, pa
 type Route
     = Home
     | Post Int
-    | ElmBlog
     | NotFound
 
 routeParser : Parser (Route -> a)a
